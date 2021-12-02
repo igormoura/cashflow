94fdf5f01443f0376cc5202b6df9cb2b723f15b3
Igor Moura <igormouradev@gmail.com>
2021-12-02 11:36:29 -0300

    config secure header

--- app/Http/Middleware/SecureHeaders.php
+++ app/Http/Middleware/SecureHeaders.php
@@ -64,7 +64,7 @@ class SecureHeaders
 
         $route = $request->route();
         if (null !== $route && 'oauth/authorize' !== $route->uri) {
-            $csp[] = "form-action 'self' https://heroku-clashflow-app.herokuapp.com 'unsafe-inline' 'unsafe-eval'";
+            $csp[] = "form-action 'self' 'self' http://* https://* 'unsafe-inline' 'unsafe-eval'";
         }
 
         $featurePolicies = [
